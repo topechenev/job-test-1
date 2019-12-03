4d754a4e843e8ea420d61716bd4a307f4081c59f
Timofey Pechenev <tpechenev@quantumbrains.com>
2019-12-03 19:50:36 +0300

    initial

---
+++ webapp/shared.lua
@@ -0,0 +1,9 @@
+local shared = {}
+shared.DATA = {}
+shared.STATE={}
+shared.STATE.enabled=true
+shared.STATE.overloaded=false
+shared.STATE.lastaccess=0
+shared.STATE.totalrequests=0
+
+return shared
